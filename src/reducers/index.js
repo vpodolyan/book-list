f370d0d014d8cf2d6da0675f4bfc5e61a2fe8a17
Vladimir Podolyan <vpodolyan@outlook.com>
2017-05-22 22:22:26 +0300

    First deploy on Github Pages

--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import books from './books'
-import addBookButton from './addBookButton'
-
-export default combineReducers({ books, addBookButton })
--- src/utils/colors.js
+++
@@ -1,6 +0,0 @@
-const Colors = {
-    mainBlue: '#1f8ceb',
-    darkGray: '#a3a2a2'
-}
-
-export default Colors;
